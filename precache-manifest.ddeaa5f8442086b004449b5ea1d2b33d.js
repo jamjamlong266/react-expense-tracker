d17058a806313e2aaff45a78dd9979777edab6b2
jamjamlong266 <jamjam@jaminpie.me>
2020-11-12 19:58:07 +0800

    Updates

---
+++ precache-manifest.ddeaa5f8442086b004449b5ea1d2b33d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938605aac99cb7b237fd71149d0d7eac",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "89ba3a6c996175fcd3a1",
+    "url": "/react-expense-tracker/static/css/main.69ff2c53.chunk.css"
+  },
+  {
+    "revision": "82adbdbabc7e0e76f795",
+    "url": "/react-expense-tracker/static/js/10.68ca7cb5.chunk.js"
+  },
+  {
+    "revision": "e98ae29568cb78429480",
+    "url": "/react-expense-tracker/static/js/2.ced5453c.chunk.js"
+  },
+  {
+    "revision": "f3ff25814f58ccca21df8c247050ac1a",
+    "url": "/react-expense-tracker/static/js/2.ced5453c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845df3616367cacb9443",
+    "url": "/react-expense-tracker/static/js/3.e3d8abf9.chunk.js"
+  },
+  {
+    "revision": "107bf0668d436dc6e3c6",
+    "url": "/react-expense-tracker/static/js/4.99b38549.chunk.js"
+  },
+  {
+    "revision": "87cbd5c2346a88cef2bd",
+    "url": "/react-expense-tracker/static/js/5.28395ec5.chunk.js"
+  },
+  {
+    "revision": "e58adce9d277b55f6c85",
+    "url": "/react-expense-tracker/static/js/6.5b8ee414.chunk.js"
+  },
+  {
+    "revision": "bf27e264f285dcb66bf4",
+    "url": "/react-expense-tracker/static/js/7.0823f444.chunk.js"
+  },
+  {
+    "revision": "b8ec8fe9c9b26115658b",
+    "url": "/react-expense-tracker/static/js/8.66624544.chunk.js"
+  },
+  {
+    "revision": "466277de203d0d2196af",
+    "url": "/react-expense-tracker/static/js/9.e1408ff7.chunk.js"
+  },
+  {
+    "revision": "89ba3a6c996175fcd3a1",
+    "url": "/react-expense-tracker/static/js/main.cb94721c.chunk.js"
+  },
+  {
+    "revision": "af651674cd916df098df",
+    "url": "/react-expense-tracker/static/js/runtime-main.23dd1317.js"
+  },
+  {
+    "revision": "32e7c234b66554bb1af0dac7ee77eff5",
+    "url": "/react-expense-tracker/static/media/no-transactions.32e7c234.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d31f690eaeb49bcd50b9cc0235770586.js"
+  "/react-expense-tracker/precache-manifest.ddeaa5f8442086b004449b5ea1d2b33d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-expense-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
